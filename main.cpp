6b3a59cbb750daeb436a14ffd914f633b442c08e
soltys22 <252252@edu.p.lodz.pl>
2024-04-14 13:15:35 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -5,9 +5,7 @@ void usage(char *programName) {
     printf("Usage: %s number1 operation number2\n", programName);
     printf("Available operations:\n");
     printf("\tadd\n");
-    printf("\tsub\n");
-    printf("\tmul\n");
-    printf("\tdiv\n");
+
     printf("\n");
 }
 
@@ -21,26 +19,13 @@ int main(int argc, char* argv[]) {
         number1 = atoi(argv[1]);
         number2 = atoi(argv[3]);
 
-        if (!strcmp("div", argv[2])) {
-            result = number1 / number2;
-            printf("%d / %d = %d\n", number1, number2, result);
-            return 0;
-        }
+
         if (!strcmp("add", argv[2])) {
             result = number1 + number2;
             printf("%d + %d = %d\n", number1, number2, result);
             return 0;
         }
-        if (!strcmp("mul", argv[2])) {
-            result = number1 * number2;
-            printf("%d * %d = %d\n", number1, number2, result);
-            return 0;
-        }
-        if (!strcmp("sub", argv[2])) {
-            result = number1 - number2;
-            printf("%d - %d = %d\n", number1, number2, result);
-            return 0;
-        }
+
     }
 
     usage(argv[0]);
